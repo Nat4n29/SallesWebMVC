a51a9d232ee24358efc457286eb2ea63cd46d15c
Nat4n29 <natan111n@hotmail.com>
2023-11-10 20:39:51 -0300

    v0.01 - Created Project
    In this commit i created the WebMvc project!

---
+++ SallesWebMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SallesWebMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
